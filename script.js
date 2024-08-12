a2834805c7270a5c86f38e1e0b8a559bd3f6053f
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-12 12:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  if(s1.includes(/s2/gi)){
+	  return s1.indexof(/s2/gi);
+  }
+	else{
+		return -1;
+	}
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
